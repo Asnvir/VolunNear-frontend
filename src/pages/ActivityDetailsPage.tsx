1712eae913169c0593ae6b2354df21a9e08bb2ab
Artur Kopytin <51398263+Asnvir@users.noreply.github.com>
2024-08-13 14:02:20 +0300

    Fixed ActivityDetailsPage (#35)

--- src/pages/ActivityDetailsPage.tsx
+++ src/pages/ActivityDetailsPage.tsx
@@ -1,16 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import { useLocation} from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 import {
-  Box,
-  VStack,
   Alert,
   AlertIcon,
+  Box,
   Divider,
+  Flex,
   Heading,
   Text,
- Flex,
+  VStack,
 } from '@chakra-ui/react';
-import { Activity } from '../api/types';
+import {Activity} from '../api/types';
 import Description from '../components/activities/activityDetails/Description.tsx';
 import SimilarListings from '../components/activities/activityDetails/SimilarListings.tsx';
 import MapComponent from '../components/activities/activityDetails/MapComponent.tsx';
@@ -19,19 +19,19 @@ import AssignToActivity from '../components/activities/activityDetails/AssignToA
 import OrganizationInfo from '../components/activities/activityDetails/OrganistaionInfo.tsx';
 import OrganisationDetails from '../components/activities/activityDetails/OrganisationDetails.tsx';
 import {useGetAverageRating} from '../hooks/organizations/useGetAvarageRating/useGetAverageRating.ts';
-import {useGetActivities} from '../hooks/activities/useGetActivities/useGetActivities.ts';
-
+import {useGetVolunteerActivities} from '../hooks/activities/useGetVolunteerActivities/useGetVolunteerActivities.ts';
 
 interface LocationState {
   activity: Activity;
 }
 
 const ActivityDetailsPage: React.FC = () => {
-  const { state } = useLocation<LocationState>();
+  const {state} = useLocation<LocationState>();
   const activity = state?.activity;
 
   // Fetch average rating
-  const { data: averageRatingData, isLoading: isLoadingRating } = useGetAverageRating(activity?.organisationId || '');
+  const {data: averageRatingData, isLoading: isLoadingRating} =
+    useGetAverageRating(activity?.organisationId || '');
   const [averageRating, setAverageRating] = useState(0);
 
   useEffect(() => {
@@ -45,9 +45,8 @@ const ActivityDetailsPage: React.FC = () => {
     data: similarActivities,
     isLoading: isLoadingActivities,
     error: errorActivities,
-  } = useGetActivities({
-    isMyActivities: false,
-    filters: { type: activity?.activityKind, date: ''},
+  } = useGetVolunteerActivities({
+    filters: {type: activity?.activityKind, date: ''},
   });
 
   if (!activity) {
@@ -65,9 +64,16 @@ const ActivityDetailsPage: React.FC = () => {
         <Heading as="h1" size="2xl" mb="4" textAlign="start">
           {activity.activityTitle}
         </Heading>
-        <CustomImageGallery galleryImages={activity.activityGalleryImages} coverImage={activity.activityCoverImage} />
-        <Flex direction={{ base: 'column', md: 'row' }} gap={6} align="flex-start">
-          <Box flex="1" maxW={{ base: '100%', md: '60%' }}>
+        <CustomImageGallery
+          galleryImages={activity.activityGalleryImages}
+          coverImage={activity.activityCoverImage}
+        />
+        <Flex
+          direction={{base: 'column', md: 'row'}}
+          gap={6}
+          align="flex-start"
+        >
+          <Box flex="1" maxW={{base: '100%', md: '60%'}}>
             <VStack align="flex-start" spacing={4}>
               <Box>
                 <Text fontSize="lg" fontWeight="bold">
@@ -92,7 +98,9 @@ const ActivityDetailsPage: React.FC = () => {
             <Description description={activity.activityDescription} />
             <Divider my={4} />
             <Box mt={4} my={6}>
-              <Heading as="h3" size="lg">Posted By</Heading>
+              <Heading as="h3" size="lg">
+                Posted By
+              </Heading>
               <OrganisationDetails
                 avatarUrl={activity.organisationAvatarUrl}
                 name={activity.organisationName}
@@ -106,13 +114,19 @@ const ActivityDetailsPage: React.FC = () => {
             <AssignToActivity activityId={activity.activityId} />
           </Box>
         </Flex>
-        <Heading as="h3" size="lg">Location</Heading>
+        <Heading as="h3" size="lg">
+          Location
+        </Heading>
         <Box>
           <Text fontSize="lg" fontWeight="bold">
-            {activity.activityCountry}, {activity.activityCity}, {activity.activityStreet}, {activity.activityNumberOfHouse}
+            {activity.activityCountry}, {activity.activityCity},{' '}
+            {activity.activityStreet}, {activity.activityNumberOfHouse}
           </Text>
         </Box>
-        <MapComponent latitude={activity.activityLatitude} longitude={activity.activityLongitude} />
+        <MapComponent
+          latitude={activity.activityLatitude}
+          longitude={activity.activityLongitude}
+        />
         <Divider my={4} />
         <SimilarListings
           activities={similarActivities} // Pass the fetched similar activities
